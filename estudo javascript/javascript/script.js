39b93f6cd620e0e9771d35329acc2502868ccd2d
joaovictor-dotcom <joaovictornyse@gmail.com>
2025-03-29 22:27:52 -0300

    menu & nav (nexora web site)

---
+++ estudo javascript/javascript/script.js
@@ -0,0 +1,12 @@
+// variáveis
+// var text = "Hello World!";
+// console.log(text);
+
+// concatenação
+var nome = prompt("Qual é o seu nome? ");
+var sobrenome = prompt("Qual é o seu sobrenome? ");
+var idade = prompt("Qual é a sua idade? ");
+
+console.log(
+  "Olá, " + nome + " " + sobrenome + "! Você tem " + idade + " anos."
+);
